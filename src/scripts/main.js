d117add4dbb2b6166043143529485fdfcfcb3455
Vinicius Mellegari <vini.a.mellegari@gmail.com>
2024-06-19 13:09:09 -0300

    parte do sobre

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
-document.querySelector('.header__menu-button').addEventListener('click', function() {
-    document.querySelector('.header__nav').classList.toggle('active');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('sobre__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('sobre__tabs__button--is-active');
+        })
+    }
+})
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('sobre__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('sobre__list--is-active');
+    }
+}
